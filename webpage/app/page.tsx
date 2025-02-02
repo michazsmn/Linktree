8c110992ccc507b1de4e67c494dbb9aedcdaeee7
Michael <michael.zusmanovskiy@gmail.com>
2025-02-02 13:10:56 +0100

    bg image blur

--- webpage/app/page.tsx
+++ webpage/app/page.tsx
@@ -1,11 +1,23 @@
-import Image from "next/image";
+import './globals.css';
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[200px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 text-6xl">
-      Welcome to my page!
-      <iframe src="https://www.youtube.com/embed/DQDaSLIRsKI" allowFullScreen width="800" height="450"/>
+    <div >
+      <div className="grid grid-rows-[200px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 text-7xl"> 
+        <Image 
+          src={"/banner.jpg"}
+          alt="background"
+          layout='fill'
+
+          quality={100}
+          className="background"
+        />
+        <h1 className="text-white">Welcome to my page!</h1>
+        <iframe src="https://www.youtube.com/embed/DQDaSLIRsKI" allowFullScreen width="800" height="450"/>
+      </div>
     </div>
+
     
   );
 }
