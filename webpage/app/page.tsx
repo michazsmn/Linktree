a0919b37a0d192f844d91ebc58b21db0c47b4cdc
Michael <michael.zusmanovskiy@gmail.com>
2025-02-03 13:51:40 +0100

    add linktree button

--- webpage/app/page.tsx
+++ webpage/app/page.tsx
@@ -1,10 +1,11 @@
 import './globals.css';
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div >
-      <div className="grid grid-rows-[200px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 text-7xl"> 
+      <div className="grid items-center justify-items-center min-h-screen sm:p-20"> 
         <Image 
           src={"/banner.jpg"}
           alt="background"
@@ -12,11 +13,12 @@ export default function Home() {
           quality={100}
           className="background"
         />
-        <h1 className="text-white drop-shadow">MIKEZAY</h1>
+        <h1 className="text-white drop-shadow text-7xl">MIKEZAY</h1>
+        <Link href="https://linktr.ee/mikezay" className='button-72'>
+          Linktree
+        </Link>
         <iframe src="https://www.youtube.com/embed/DQDaSLIRsKI" allowFullScreen width="800" height="450"/>
       </div>
     </div>
-
-    
   );
 }
