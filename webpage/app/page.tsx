b69414ed021b2623cdaa5fd97ff1c17417be49b0
Michael <michael.zusmanovskiy@gmail.com>
2025-02-06 23:10:30 +0100

    styling changes

--- webpage/app/page.tsx
+++ webpage/app/page.tsx
@@ -1,15 +1,7 @@
-"use client"
-
 import './globals.css';
 import Image from 'next/image'
 import Link from 'next/link'
 import '@geist-ui/core'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
 import {
   Popover,
   PopoverContent,
@@ -20,40 +12,56 @@ import {
 
 export default function Home() {
   return (
-    <div >
-      <div className="grid items-center justify-items-center min-h-screen sm:p-20">
-
-      <Image 
+    <div className='pt-48'>
+        <Image 
           src={"/banner.jpg"}
           alt="background"
           fill={true}
           quality={100}
           className="background"
         />
+      <div className="grid items-center gap-10 justify-items-center">
 
-        <h1 className="text-white drop-shadow text-7xl">MIKEZAY</h1>
+        <Image
+          src={"/mikezayeffects.jpg"}
+          alt="profilbild"
+          quality={100}
+          width={120}
+          height={120}
+          className='profilepic'
+        />
 
-        <Popover>
-          <PopoverTrigger className='button-72'>Youtube</PopoverTrigger>
-          <PopoverContent>
-            <div className='justify-items-center'>
-                    <iframe src="https://www.youtube.com/embed/DQDaSLIRsKI" width="800" height="450"/>
-            </div>
-          </PopoverContent>
-        </Popover>
+        <div>
+          <h1 className="text-stone-200 drop-shadow text-xl">MIKEZAY</h1>
+          <h1 className="text-stone-400 drop-shadow text-xl">producer</h1>
+        </div>
+        
+        <div className="grid items-center gap-6">
 
-        <Popover>
-          <PopoverTrigger className='button-72'>Spotify</PopoverTrigger>
-          <PopoverContent>
-            <div>
-              <iframe src="https://open.spotify.com/embed/artist/5b5IWd6FFzq9Ij6etGsktH?utm_source=generator" width="100%" height="352" 
-                  frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            </div>
-          </PopoverContent>
-        </Popover>
+          <Popover>
+            <PopoverTrigger className='button-27'>Youtube</PopoverTrigger>
+            <PopoverContent className='bg-black'>
+              <div className='justify-items-center'>
+                      <iframe src="https://www.youtube.com/embed/DQDaSLIRsKI" width="640" height="360"/>
+              </div>
+            </PopoverContent>
+          </Popover>
 
-        <Link href="https://www.instagram.com/micha_zsmn/" className='button-72'>Instagram</Link>
+          <Popover>
+            <PopoverTrigger className='button-27'>Spotify</PopoverTrigger>
+            <PopoverContent className='bg-black'> 
+              <div>
+                <iframe src="https://open.spotify.com/embed/artist/5b5IWd6FFzq9Ij6etGsktH?utm_source=generator" width="100%" height="400" 
+                    frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+            </PopoverContent>
+          </Popover>
 
+          <Link href="https://www.instagram.com/micha_zsmn/" className='button-27'>Instagram</Link>
+
+        </div>
+
+        
       </div>
     </div>
   );
